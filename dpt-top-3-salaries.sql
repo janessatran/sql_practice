4633a61818a1cdf3e3bbb6ff71e817e2c7d1eb94
Janessa Tran <tranjanessa@gmail.com>
2018-08-18 11:25:38 -0700

    Update dpt-top-3-salaries.sql

--- dpt-top-3-salaries.sql
+++ dpt-top-3-salaries.sql
@@ -58,3 +58,18 @@ select
 select r.Department, r.Employee, r.Salary
 from r
 where rank <= 3
+
+
+# Write your MySQL query statement below
+SELECT 
+    d.Name AS Department, e.Name AS Employee, e.Salary
+FROM 
+    Employee e
+JOIN 
+    Department d on e.DepartmentId = d.Id
+WHERE (
+    SELECT COUNT(DISTINCT Salary)
+    FROM Employee
+    WHERE Salary > e.Salary
+    AND DepartmentId = d.Id) < 3
+ORDER BY d.Name, e.Salary DESC
